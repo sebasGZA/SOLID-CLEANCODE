b7a187ed38a72ec448ea0160acb3be6add2c4221
sebasGZA <sebassga34@gmail.com>
2023-02-09 11:20:02 -0500

    03-functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -37,4 +37,16 @@
     console.log("Crear actor");
     return true;
   }
+
+  //Goes on
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    //return isDead ? 1500 : isSeparated ? 2500 : isRetired ? 3000 : 4000;
+    if (isDead) return 1500;
+    if (isSeparated) return 2500;
+    return isRetired ? 3000 : 4000;
+  };
 })();
