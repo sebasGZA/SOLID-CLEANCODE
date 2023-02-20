d4fd04e41d7c88a67079041aa59259b72e063de1
sebasGZA <sebassga34@gmail.com>
2023-02-19 19:26:41 -0500

    02-SOLID:ocp

---
+++ src/solid/03-open-close.ts
@@ -0,0 +1,11 @@
+// import axios from "axios";
+
+export class HttpClient {
+  async get(url: string) {
+    // const { data, status } = await axios.get(url);
+    // console.log(status);
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return { data, status: resp.status };
+  }
+}
