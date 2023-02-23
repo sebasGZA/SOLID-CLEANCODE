05b0f8c806201ad3558f19ad565264a144741d1b
sebasGZA <sebassga34@gmail.com>
2023-02-23 13:20:45 -0500

    SOLID:04-Interface segregation

---
+++ src/solid/05-dependency-a.ts
@@ -0,0 +1,12 @@
+import { PostService } from "./05-dependency-b";
+import { WebApiPostService } from "./05-dependency-c";
+
+// Main
+(async () => {
+  const provider = new WebApiPostService();
+  const postService = new PostService(provider);
+
+  const posts = await postService.getPosts();
+
+  console.log({ posts });
+})();
